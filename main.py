6825f46934d672d259da55fadab45e418af26ad9
Sumit Chaudhary <chaudharysumit130@gmail.com>
2025-12-06 13:37:24 +0530

    File uplading and saving code written

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, status, HTTPException, UploadFile
 
 # to run this file fastapi application , use the command: fastapi dev main.py
 
@@ -9,6 +9,8 @@ app = FastAPI()
 uploads_dir = Path("./uploads")
 uploads_dir.mkdir(exist_ok=True)
 
+ALLOWED_MIME_TYPES = ["application/pdf"]
+
 
 @app.get("/")
 def root():
@@ -18,19 +20,40 @@ def root():
 # api to upload evidence files
 # currently it accepts client id as query parameter and creates a folder inside upload folder with client id name
 @app.post("/uploadEvidence", status_code=status.HTTP_201_CREATED)
-def upload_evidence(client_id: str):
+async def upload_evidence(client_id: str, file: UploadFile):
+
+    if file.content_type not in ALLOWED_MIME_TYPES:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            details=f"Invalid file type. Allowed: {ALLOWED_MIME_TYPES}",
+        )
+
     # Creates a directory for a specific client_id inside 'uploads'.
     try:
         client_upload_dir = uploads_dir / client_id
-        # create directory if not exists
+
+        # create directory including parent directory if not exists
         client_upload_dir.mkdir(parents=True, exist_ok=True)
 
-        return {"message": f"Evidence files uploaded for client {client_id}"}
+        # Define the final file path (Folder + Filename)
+        # file.filename comes from the user's computer
+        file_path = client_upload_dir / file.filename
+
+        # --- STEP 3: SAVE THE FILE ---
+        # We use 'with' (Context Manager) to ensure the file closes properly after writing
+        # "wb" means "Write Binary" (essential for images/PDFs)
+        with open(file_path, "wb") as file_object:
+
+            content = await file.read()
+            file_object.write(content)
+
+        return {"message": f"{file.filename} uploaded successfully"}
 
     except PermissionError:
+        # if script don't have permission to create directory at given location
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Server permission denied. Cannot create folder",
+            detail="Server permission denied. Cannot create folder or cannot save file",
         )
 
     except Exception as e:
